74de9591318b831a74746ded5f04df6968453b06
Jacob <j31c0@hotmail.com>
2022-11-13 20:22:16 -0500

    Add more methods
    Use pop, push, shift and unshift, splice and slice

--- js-fourth-part/script.js
+++ js-fourth-part/script.js
@@ -38,4 +38,15 @@ colors.pop(); // Removes the last element
 colors.push("white"); // Adds a new element (at the end)
 colors.shift(); // Removes the first array element
 colors.unshift("green"); // Adds a new element (at the beginning);
-colors[colors.length] = "cyan"; //Adds a new element using length
+colors[colors.length] = "cyan"; // Adds a new element using length
+
+const arr1 = ["Cecilie", "Lone"];
+const arr2 = ["Emil", "Tobias", "Linus"];
+const arr3 = ["Robin", "Morgan"];
+const myChildren = arr1.concat(arr2, arr3); // Concatenate arrays without changing them. Also It could work with values "concat('Peter')"
+myChildren.splice(2, 1, "Will", "Smith"); // Splice can be used to add new items, 2 defines the position where new elements should be added, 1 defines how many elements should me removes, ("Will", "Smith") define the new elements to be added
+myChildren.splice(1,2);
+console.log(myChildren);
+
+const lightColor = colors.slice(1); // Slice out a piece of an array into a new one, being (1) the starting index
+console.log(lightColor);
