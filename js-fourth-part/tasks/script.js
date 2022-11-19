21e1080ab8149f994471cfdbe897f75233913fa7
Jacob <j31c0@hotmail.com>
2022-11-19 15:27:03 -0500

    Add third  and solution

--- js-fourth-part/tasks/script.js
+++ js-fourth-part/tasks/script.js
@@ -7,11 +7,18 @@ while (i) {
 
 
 /* Second task */
-let i = 0;
+/* let i = 0;
 while (++i < 5) alert( i ); // 1, 2, 3, 4
 
 let j = 0;
 while (j++ < 5) alert( j ); // 1, 2, 3, 4, 5
+ */
 
 
 /* Third task */
+for (let i = 0; i < 5; i++) alert( i );
+// 0, 1, 2, 3, 4
+
+for (let j = 0; j < 5; ++j) alert( j );
+// 0, 1, 2, 3, 4
+
