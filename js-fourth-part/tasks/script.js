0687f5f3e8470d2b3d33e75260c644a381382a44
Jacob <j31c0@hotmail.com>
2022-11-19 00:10:04 -0500

    Add second task solution

--- js-fourth-part/tasks/script.js
+++ js-fourth-part/tasks/script.js
@@ -1,5 +1,17 @@
-let i = 3;
+/* First task */
+/* let i = 3;
 
 while (i) {
-  alert( i-- ); /* Last value alerted is 1 */
-}
+  alert( i-- ); // Last value alerted is 1
+} */
+
+
+/* Second task */
+let i = 0;
+while (++i < 5) alert( i ); // 1, 2, 3, 4
+
+let j = 0;
+while (j++ < 5) alert( j ); // 1, 2, 3, 4, 5
+
+
+/* Third task */
