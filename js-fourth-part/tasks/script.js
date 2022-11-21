20d024560cdc15456b397d734d497a8993387038
Jacob <j31c0@hotmail.com>
2022-11-21 01:27:35 -0500

    Add final task

--- js-fourth-part/tasks/script.js
+++ js-fourth-part/tasks/script.js
@@ -46,14 +46,29 @@ for (let i = 0; i < 3; i++) {
 
 
 /* Sixth task */
-let number = prompt("Type a number grater than 100");
+/* let number = prompt("Type a number grater than 100");
 while (number < 100) {
   if (number == "" || number == null) break;
   number = prompt("Type a number grater than 100");
 }
 
-/* let num;
+let num;
 do {
   num = prompt("Enter a number greater than 100?", 0);
-} while (num <= 100 && num); */
+} while (num <= 100 && num);
+*/
 
+
+/* Final task */
+let n = 10;
+let j = 0;
+
+for (let i = 1; i < n; i++) {
+  if (n % i == 0) {
+    j++;
+    console.log(i);
+  }
+  if (i % i == 0 && j < 2) {
+    console.log(i)
+  }
+}
