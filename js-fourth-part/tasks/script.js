75c3dfbf0c08494cd8ec8fea8c949589540074db
Jacob <j31c0@hotmail.com>
2022-11-21 15:56:29 -0500

    Add another solution

--- js-fourth-part/tasks/script.js
+++ js-fourth-part/tasks/script.js
@@ -72,6 +72,20 @@ for (let i = 1; i < n; i++) {
     j++;
   }
   if (remainCounter == 2) {
-    console.log(i);
+    alert(i);
   }
 }
+
+/* 
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) { // for each i...
+
+  for (let j = 2; j < i; j++) { // look for a divisor..
+    if (i % j == 0) continue nextPrime; // not a prime, go next i
+  }
+
+  alert( i ); // a prime
+}
+ */
