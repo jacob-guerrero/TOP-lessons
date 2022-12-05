fb359e3ee2f4d22a48913ee49e49b576c4244935
Jacob <j31c0@hotmail.com>
2022-12-05 02:07:26 -0500

    Edit attributes on a new div

--- js-dom/script.js
+++ js-dom/script.js
@@ -1,3 +1,4 @@
+// Targeting nodes with selectors
 const container = document.querySelector('#container');
 console.dir(container.firstElementChild);
 
@@ -10,14 +11,22 @@ console.dir(container.lastElementChild);
 const allSelectors = document.querySelectorAll('container');
 console.dir(container); // Contains a nodelist
 
-const div = document.createElement('div');
+
+// DOM Methods
+const div = document.createElement('div'); // Create Elements
 const textDiv =document.createTextNode('This was created first using JavaScript');
-div.appendChild(textDiv);
-container.appendChild(div);
+div.appendChild(textDiv); // Add text
+container.appendChild(div); // Append elements
 
 const div2 = document.createElement('div');
 const textDiv2 = document.createTextNode('This was created second using JS');
 div2.appendChild(textDiv2);
 const firstDiv = document.querySelector('.display');
 container.insertBefore(div2, firstDiv);
+
+
+// Editing attributes
+const divAttribute = document.createElement('div');
+divAttribute.setAttribute('id', 'theDiv');
+container.appendChild(divAttribute);
 
