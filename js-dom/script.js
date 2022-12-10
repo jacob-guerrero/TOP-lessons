739d2fbf2d11fdd3979060226b8bd19bc8f1a8f5
Jacob <j31c0@hotmail.com>
2022-12-10 12:38:45 -0500

    Add third  solution

--- js-dom/script.js
+++ js-dom/script.js
@@ -59,3 +59,18 @@ const h3= document.createElement('h3');
 h3.textContent = "I'm a blue h3!";
 h3.style.cssText = 'color: blue;';
 container.appendChild(h3);
+
+const divParent = document.createElement('div');
+divParent.setAttribute('style', 'border: 1px solid black; background-color: pink');
+/* divParent.textContent = "hi"; // Test only */
+/* container.appendChild(divParent); */
+
+const h1Div = document.createElement('h1');
+h1Div.textContent = "I'm in a div";
+divParent.appendChild(h1Div);
+
+pDiv = document.createElement('p');
+pDiv.textContent = "ME TOO!";
+divParent.appendChild(pDiv);
+
+container.appendChild(divParent);
