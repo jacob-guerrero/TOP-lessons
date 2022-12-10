64c3a7ffadd4eddb8841270d0d5f266e16d18713
Jacob <j31c0@hotmail.com>
2022-12-09 20:12:54 -0500

    Add second solution

--- js-dom/script.js
+++ js-dom/script.js
@@ -54,3 +54,8 @@ const p = document.createElement('p');
 p.textContent = "Hey I'm red!";
 p.style.color = 'red';
 container.appendChild(p);
+
+const h3= document.createElement('h3');
+h3.textContent = "I'm a blue h3!";
+h3.style.cssText = 'color: blue;';
+container.appendChild(h3);
