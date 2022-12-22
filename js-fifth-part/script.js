8aa192c54d30fcd7694138bcc3fd29362e7d642b
Jacob <j31c0@hotmail.com>
2022-12-22 17:01:10 -0500

    Use square brackets to define complex keys

--- js-fifth-part/script.js
+++ js-fifth-part/script.js
@@ -6,3 +6,14 @@ let user = {
 }
 delete user.error;
 console.log(user);
+
+//let key = prompt("What info do you want?", 'name');
+//alert(user[key]);
+
+//complex expressions must use square brackets:
+let fruit = prompt('what fruit do you want?', 'apple');
+let bag = {
+  [fruit + 'Computers']: 5
+};
+console.log(bag['appleComputers'])
+
