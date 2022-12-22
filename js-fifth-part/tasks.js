dab7abebd9e86949754322649d1b2d7b50453f8d
Jacob <j31c0@hotmail.com>
2022-12-22 18:54:40 -0500

    Solve task 2

--- js-fifth-part/tasks.js
+++ js-fifth-part/tasks.js
@@ -4,3 +4,20 @@ user.name = "John";
 user['surname'] = "Smith";
 user.name = "Pete";
 delete user['name'];
+
+
+// Check for emptiness
+function isEmpty(obj) {
+    for (let prop in obj) {
+        return false
+    }
+    return true
+}
+
+let schedule = {};
+console.log( isEmpty(schedule) ); // true
+schedule["8:30"] = "get up";
+
+console.log( isEmpty(schedule) ); // false
+
+
