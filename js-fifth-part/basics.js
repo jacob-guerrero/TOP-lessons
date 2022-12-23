7a22809d6ab1123526241f0ed1381e4553dbfe3e
Jacob <j31c0@hotmail.com>
2022-12-22 22:40:03 -0500

    Add object basics exercises

---
+++ js-fifth-part/basics.js
@@ -0,0 +1,16 @@
+//Object basics 1
+const cat = {
+    name : 'Bertie',
+    breed : 'Cymric',
+    color : 'white',
+    greeting: function() {
+      console.log('Meow!');
+    }
+  }
+  
+  // Put your code here
+  let catName = cat['name'];
+  cat.greeting();
+  cat.color = 'black';
+  // Don't edit the code below here
+
