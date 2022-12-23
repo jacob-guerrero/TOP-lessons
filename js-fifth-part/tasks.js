d0b362436ec97fc8b21a02046683f207adb65b60
Jacob <j31c0@hotmail.com>
2022-12-22 19:10:35 -0500

    Solve 3rd task

--- js-fifth-part/tasks.js
+++ js-fifth-part/tasks.js
@@ -21,3 +21,15 @@ schedule["8:30"] = "get up";
 console.log( isEmpty(schedule) ); // false
 
 
+// Sum object properties
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+let sum = 0;
+for (let prop in salaries) {
+    sum += salaries[prop]
+}
+console.log(sum);
