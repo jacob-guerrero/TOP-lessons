5a0828a930b7d1532c11f55287130b042236a1c5
Jacob <j31c0@hotmail.com>
2023-02-23 23:59:01 -0500

    Add this in method invocation and separating it

--- js-full/object-constructor/this.js
+++ js-full/object-constructor/this.js
@@ -1,5 +1,5 @@
 /* Function Invocation */
-function hello(name) {
+/* function hello(name) {
   return "Hello " + name + "!";
 }
 
@@ -30,4 +30,54 @@ const numbers = {
     return calculate();
   },
 };
-console.log(numbers.sum()); // => 15
+console.log(numbers.sum()); // => 15 */
+
+// -----------
+
+/* Method invocation */
+const myObject = {
+  // helloMethod is a method
+  helloMethod: function () {
+    return "Hello World!";
+  },
+};
+const message = myObject.helloMethod();
+console.log(message);
+
+// Using this
+const calc = {
+  num: 0,
+  increment() {
+    console.log(this === calc); // => true
+    this.num += 1;
+    return this.num;
+  },
+};
+// method invocation. this is calc
+console.log(calc.increment()); // => 1
+console.log(calc.increment()); // => 2
+
+// Inherits prototype
+const myDog = Object.create({
+  sayName() {
+    console.log(this === myDog); // => true
+    return this.name;
+  },
+});
+myDog.name = "Milo";
+// method invocation. this is myDog
+console.log(myDog.sayName()); // => 'Milo'
+
+// Using arrow function
+function Pet(type, legs) {
+  this.type = type;
+  this.legs = legs;
+  this.logInfo = () => {
+    console.log(this === myCat); // => true
+    console.log(`The ${this.type} has ${this.legs} legs`);
+  };
+}
+const myCat = new Pet("Cat", 4);
+// logs "The Cat has 4 legs"
+setTimeout(myCat.logInfo, 1000);
+// myCat.logInfo returns a new function that executes exactly like logInfo, but has this as myCat, even in a function invocation.
