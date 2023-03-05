12f7b97275bb34b6461c2b6a10b1228c8ab4a1f5
Jacob <j31c0@hotmail.com>
2023-03-05 16:51:14 -0500

    Add more private variables and functions examples

--- js-full/factory-functions/script.js
+++ js-full/factory-functions/script.js
@@ -130,4 +130,34 @@ const Module3 = (function () {
   return myModule; // returns the Object with public methods
 })();
 // usage
-Module.publicMethod();
+Module3.publicMethod();
+
+/* Private variables and functions */
+const FactoryFunction = string => {
+  const capitalizeString = () => string.toUpperCase();
+  const printString = () => console.log(`----${capitalizeString()}----`);
+  return { printString };
+};
+
+const taco = FactoryFunction('taco');
+
+// printString(); // ERROR!!
+// capitalizeString(); // ERROR!!
+// taco.capitalizeString(); // ERROR!!
+taco.printString(); // this prints "----TACO----"
+
+// Another example:
+const counterCreator = () => {
+  let count = 0;
+  return () => {
+    console.log(count);
+    count++;
+  };
+};
+
+const counter = counterCreator();
+
+counter(); // 0
+counter(); // 1
+counter(); // 2
+counter(); // 3
