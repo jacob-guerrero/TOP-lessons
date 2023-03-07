dc09a03202850ebdda38227e9d1c7619147a219d
Jacob <j31c0@hotmail.com>
2023-03-06 19:48:57 -0500

    Add IIFE example

--- js-full/factory-functions/module.js
+++ js-full/factory-functions/module.js
@@ -16,3 +16,17 @@ calculator.add(3, 5); // 8
 calculator.sub(6, 2); // 4
 calculator.mul(14, 5534); // 77476
 
+// IIFE Immediately invoked function expression
+// Anything within the parentheses is part of an expression
+(function () {
+  /* logic here */
+});
+
+// IIFE has a couple of parentheses on the end.
+(function () {
+  const foo = "bar";
+
+  // Outputs: "bar"
+  console.log(foo);
+})();
+console.log(foo); // ReferenceError: foo is not defined
