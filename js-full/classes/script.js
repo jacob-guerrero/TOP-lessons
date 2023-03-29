bc23e44ee8a5cce9e0107d0bd2e5b1ee60769520
Jacob <j31c0@hotmail.com>
2023-03-29 11:08:07 -0500

    Add example using getters and setters

---
+++ js-full/classes/script.js
@@ -0,0 +1,29 @@
+let obj = {
+  get propName() {
+    // getter, the code executed on getting obj.propName
+  },
+
+  set propName(value) {
+    // setter, the code executed on setting obj.propName = value
+  },
+};
+
+// First example
+let user = {
+  name: "John",
+  surname: "Smith",
+
+  get fullName() {
+    return `${this.name} ${this.surname}`;
+  },
+
+  set fullName(value) {
+    [this.name, this.surname] = value.split(" ");
+  },
+};
+
+// set fullName is executed with the given value.
+user.fullName = "Alice Cooper";
+
+alert(user.name); // Alice
+alert(user.surname); // Cooper
