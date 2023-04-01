1884977b32fe3c6a0ab91411407f6d9f945d1097
Jacob <j31c0@hotmail.com>
2023-04-01 18:49:38 -0500

    Add mixin example

--- js-full/classes/mdnScript.js
+++ js-full/classes/mdnScript.js
@@ -1,65 +1,81 @@
 /* Inheritance in classes using extends*/
 class Animal {
-    constructor(name) {
-      this.name = name;
-    }
-  
-    speak() {
-      console.log(`${this.name} makes a noise.`);
-    }
+  constructor(name) {
+    this.name = name;
   }
-  
-  class Dog extends Animal {
-    constructor(name) {
-      super(name); // call the super class constructor and pass in the name parameter
-    }
-  
-    speak() {
-      console.log(`${this.name} barks.`);
-    }
+
+  speak() {
+    console.log(`${this.name} makes a noise.`);
   }
-  
-  const d = new Dog("Mitzie");
-  d.speak(); // Mitzie barks.
-  
+}
+
+class Dog extends Animal {
+  constructor(name) {
+    super(name); // call the super class constructor and pass in the name parameter
+  }
+
+  speak() {
+    console.log(`${this.name} barks.`);
+  }
+}
+
+const d = new Dog("Mitzie");
+d.speak(); // Mitzie barks.
+
 // Second example
-  class Cat {
-    constructor(name) {
-      this.name = name;
-    }
-  
-    speak() {
-      console.log(`${this.name} makes a noise.`);
-    }
+class Cat {
+  constructor(name) {
+    this.name = name;
   }
-  
-  class Lion extends Cat {
-    speak() {
-      super.speak();
-      console.log(`${this.name} roars.`);
-    }
+
+  speak() {
+    console.log(`${this.name} makes a noise.`);
   }
-  
-  const l = new Lion("Fuzzy");
-  l.speak();
-  // Fuzzy makes a noise.
-  // Fuzzy roars.
-  
-  /* Extending plain objects */
-  const AnimalPO = {
-    speak() {
-      console.log(`${this.name} makes a noise.`);
-    },
+}
+
+class Lion extends Cat {
+  speak() {
+    super.speak();
+    console.log(`${this.name} roars.`);
+  }
+}
+
+const l = new Lion("Fuzzy");
+l.speak();
+// Fuzzy makes a noise.
+// Fuzzy roars.
+
+/* Extending plain objects */
+const AnimalPO = {
+  speak() {
+    console.log(`${this.name} makes a noise.`);
+  },
+};
+
+class DogPO {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+Object.setPrototypeOf(DogPO.prototype, AnimalPO);
+
+const dPO = new DogPO("Mitzie");
+dPO.speak(); // Mitzie makes a noise.
+
+// -----------------
+
+/* Mixins */
+const calculatorMixin = (Base) =>
+  class extends Base {
+    calc() {}
   };
-  
-  class DogPO {
-    constructor(name) {
-      this.name = name;
-    }
-  }
-  
-  Object.setPrototypeOf(DogPO.prototype, AnimalPO);
-  
-  const dPO = new DogPO("Mitzie");
-  dPO.speak(); // Mitzie makes a noise.
-  
+
+const randomizerMixin = (Base) =>
+  class extends Base {
+    randomize() {}
+  };
+
+// Using the mixins:
+class Foo {}
+class Bar extends calculatorMixin(randomizerMixin(Foo)) {}
