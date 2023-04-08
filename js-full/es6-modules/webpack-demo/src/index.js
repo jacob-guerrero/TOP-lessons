fe994354e1b54944ba93dc5af9c5887e8404314c
Jacob <j31c0@hotmail.com>
2023-04-08 17:19:52 -0500

    Add example using import

--- js-full/es6-modules/webpack-demo/src/index.js
+++ js-full/es6-modules/webpack-demo/src/index.js
@@ -1,11 +1,18 @@
-import _ from 'lodash';
+// Import new Function
+import myName from "./myName";
+
+import _ from "lodash";
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement("div");
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  const elementName = document.createElement("div");
+  elementName.textContent = myName("Cody");
+
+  return { element, elementName };
+}
+
+document.body.appendChild(component().element);
+document.body.appendChild(component().elementName);
