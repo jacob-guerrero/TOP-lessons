7a393ffa54e1c8d11c0a62744155901befb8d749
Jacob <j31c0@hotmail.com>
2023-06-13 22:23:52 -0500

    Add transitions to the img using classes

--- js-full/ui-interactions/drop-down-menu/script.js
+++ js-full/ui-interactions/drop-down-menu/script.js
@@ -18,25 +18,31 @@ const updateContent = () => {
 window.addEventListener("resize", updateContent);
 updateContent();
 
-const pictures = document.querySelectorAll(".picture");
-let index = 0;
-const nextPicture = () => {
-  index += 1;
-  if (index >= pictures.length) {
-    index = 0;
-  }
-  if (index < pictures.length) {
-    document.querySelector(".show").classList.remove("show");
-    pictures[index].classList.add("show");
-  }
-};
-const previousPicture = () => {
-  index -= 1;
-  if (index < 0) {
-    index = pictures.length - 1;
-  }
-  if (index >= 0) {
-    document.querySelector(".show").classList.remove("show");
-    pictures[index].classList.add("show");
-  }
-}
+const slider = (() => {
+  const pictures = document.querySelectorAll(".picture");
+  let index = 0;
+
+  const nextPicture = () => {
+    index += 1;
+
+    if (index >= pictures.length) {
+      index = 0;
+    }
+    if (index < pictures.length) {
+      document.querySelector(".show").classList.remove("show");
+      pictures[index].classList.add("show");
+    }
+  };
+  const previousPicture = () => {
+    index -= 1;
+
+    if (index < 0) {
+      index = pictures.length - 1;
+    }
+    if (index >= 0) {
+      document.querySelector(".show").classList.remove("show");
+      pictures[index].classList.add("show");
+    }
+  };
+  return { nextPicture, previousPicture}
+})();
