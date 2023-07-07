223bc1cae52ff14f4d596007a4afb44e5fd6e310
Jacob <j31c0@hotmail.com>
2023-07-07 17:20:44 -0500

    Add form content and email validation

--- js-full/form-validation/src/index.js
+++ js-full/form-validation/src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+const email = document.querySelector("#email");
+const country = document.querySelector("#country");
+const zip = document.querySelector("#zip");
+const password = document.querySelector("#password");
+const passConfirmation = document.querySelector("#pass-confirmation");
+
+/* Validation */
+email.addEventListener("input", (e) => {
+    if(email.validity.typeMismatch) {
+        email.setCustomValidity("Type a correct email");
+    } else {
+        email.setCustomValidity("");
+    }
+})
