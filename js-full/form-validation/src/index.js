04b3a9fb39229db9b9d750e1334c7f12e3185308
Jacob <j31c0@hotmail.com>
2023-07-18 11:54:52 -0500

    Add password validation

--- js-full/form-validation/src/index.js
+++ js-full/form-validation/src/index.js
@@ -1,8 +1,6 @@
 import "./style.css";
 
 const email = document.querySelector("#email");
-const password = document.querySelector("#password");
-const passConfirmation = document.querySelector("#pass-confirmation");
 
 /* Validation */
 email.addEventListener("input", () => {
@@ -14,45 +12,73 @@ email.addEventListener("input", () => {
 });
 
 function checkZIP() {
-    const constraints = {
-      ch: [
-        "^(CH-)?\\d{4}$",
-        "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
-      ],
-      fr: [
-        "^(F-)?\\d{5}$",
-        "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
-      ],
-      de: [
-        "^(D-)?\\d{5}$",
-        "Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345",
-      ],
-      nl: [
-        "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
-        "Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
-      ],
-    };
+  const constraints = {
+    ch: [
+      "^(CH-)?\\d{4}$",
+      "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
+    ],
+    fr: [
+      "^(F-)?\\d{5}$",
+      "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
+    ],
+    de: [
+      "^(D-)?\\d{5}$",
+      "Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345",
+    ],
+    nl: [
+      "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+      "Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+    ],
+  };
 
-    // Get the values
-    const country = document.querySelector("#country").value;
-    const zip = document.querySelector("#zip");
+  // Get the values
+  const country = document.querySelector("#country").value;
+  const zip = document.querySelector("#zip");
 
-    // Build the constraint checker
-    const constraint = new RegExp(constraints[country][0], "");
-    console.log(constraint);
-  
-    // Check it!
-    if (constraint.test(zip.value)) {
-      // The ZIP follows the constraint, we use the ConstraintAPI to tell it
-      zip.setCustomValidity("");
-    } else {
-      // The ZIP doesn't follow the constraint, we use the ConstraintAPI to
-      // give a message about the format required for this country
-      zip.setCustomValidity(constraints[country][1]);
-    }
+  // Build the constraint checker
+  const constraint = new RegExp(constraints[country][0], "");
+  console.log(constraint);
+
+  // Check it!
+  if (constraint.test(zip.value)) {
+    // The ZIP follows the constraint, we use the ConstraintAPI to tell it
+    zip.setCustomValidity("");
+  } else {
+    // The ZIP doesn't follow the constraint, we use the ConstraintAPI to
+    // give a message about the format required for this country
+    zip.setCustomValidity(constraints[country][1]);
+  }
 }
 
-window.onload = () => {
-    document.getElementById("country").onchange = checkZIP;
-    document.getElementById("zip").oninput = checkZIP;
+const checkPassword = () => {
+  const password = document.querySelector("#password");
+  const passConfirmation = document.querySelector("#pass-confirmation");
+
+  if (password.validity.tooShort || password.validity.tooLong) {
+    password.setCustomValidity("Your password must have 6-12 characters");
+  } else {
+    password.setCustomValidity("");
+  }
 };
+
+const validatePasswords = () => {
+  const password = document.querySelector("#password");
+  const passConfirmation = document.querySelector("#pass-confirmation");
+  if (password.value !== passConfirmation.value) {
+    passConfirmation.setCustomValidity("Your passwords does not match!");
+  } else {
+    passConfirmation.setCustomValidity("");
+  }
+};
+
+window.onload = () => {
+  document.getElementById("country").onchange = checkZIP;
+  document.getElementById("zip").oninput = checkZIP;
+
+  document.getElementById("password").oninput = checkPassword;
+  document.getElementById("pass-confirmation").oninput = validatePasswords;
+};
+
+document.querySelector(".submit").addEventListener("click", () => {
+  validatePasswords();
+});
