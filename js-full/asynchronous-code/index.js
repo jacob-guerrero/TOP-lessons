a5aa0aa62b7cc0244b18f842b6d150e21e04d583
Jacob <j31c0@hotmail.com>
2023-07-25 11:43:30 -0500

    Add catch to handle errors and add data

--- js-full/asynchronous-code/index.js
+++ js-full/asynchronous-code/index.js
@@ -1,21 +1,40 @@
-const myDiv = document.querySelector("div#myDiv")
+const myDiv = document.querySelector("div#myDiv");
 
-myDiv.addEventListener("click", function(event){
-  console.log("You clicked me!", event)
-})
+myDiv.addEventListener("click", function (event) {
+  console.log("You clicked me!", event);
+});
 
-const array = [1,2,3,4]
+const array = [1, 2, 3, 4];
 
-array.forEach(callback)
+array.forEach(callback);
 
-function callback(arg){
-  console.log(arg * 2)
+function callback(arg) {
+  console.log(arg * 2);
 }
 
-new Promise(function(resolve, reject) { 
-	// A mock async action using setTimeout
-	setTimeout(function() { resolve(10); }, 2000);
+new Promise(function (resolve, reject) {
+  // A mock async action using setTimeout
+  /* setTimeout(function () {
+    resolve(10);
+  }, 2000); */
+  
+  setTimeout(function () {
+    reject("Error: This is a test");
+  }, 2000);
 })
-.then(function(num) { console.log('first then: ', num); return num * 2; })
-.then(function(num) { console.log('second then: ', num); return num * 2; })
-.then(function(num) { console.log('last then: ', num);});
+  .then(function (num) {
+    console.log("first then: ", num);
+    return num * 2;
+  })
+  .then(function (num) {
+    console.log("second then: ", num);
+    return num * 2;
+  })
+  .then(function (num) {
+    console.log("last then: ", num);
+  })
+  .catch(handleError);
+
+function handleError(error) {
+  console.log(error);
+}
