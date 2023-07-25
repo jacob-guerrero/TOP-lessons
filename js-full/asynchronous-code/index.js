1482877458c06b2316597afefa0e0010d233ae7a
Jacob <j31c0@hotmail.com>
2023-07-25 11:20:54 -0500

    Add promise example

--- js-full/asynchronous-code/index.js
+++ js-full/asynchronous-code/index.js
@@ -11,3 +11,11 @@ array.forEach(callback)
 function callback(arg){
   console.log(arg * 2)
 }
+
+new Promise(function(resolve, reject) { 
+	// A mock async action using setTimeout
+	setTimeout(function() { resolve(10); }, 2000);
+})
+.then(function(num) { console.log('first then: ', num); return num * 2; })
+.then(function(num) { console.log('second then: ', num); return num * 2; })
+.then(function(num) { console.log('last then: ', num);});
