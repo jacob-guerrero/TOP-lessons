dce9d43d553568d7b87c617f928bc519fe2037e8
Jacob <j31c0@hotmail.com>
2023-07-25 12:14:07 -0500

    Add solution to display cat names

--- js-full/asynchronous-code/index.js
+++ js-full/asynchronous-code/index.js
@@ -12,12 +12,13 @@ function callback(arg) {
   console.log(arg * 2);
 }
 
-new Promise(function (resolve, reject) {
+/* new Promise(function (resolve, reject) {
   // A mock async action using setTimeout
-  /* setTimeout(function () {
-    resolve(10);
-  }, 2000); */
-  
+
+  //  setTimeout(function () {
+  //    resolve(10);
+  //  }, 2000);
+
   setTimeout(function () {
     reject("Error: This is a test");
   }, 2000);
@@ -37,4 +38,25 @@ new Promise(function (resolve, reject) {
 
 function handleError(error) {
   console.log(error);
+} */
+
+/* Promises */
+const promise = fetch("data/cats.json")
+.then(convertToJson)
+.then(displayNames);
+
+console.log("Our promise: ", promise);
+
+function convertToJson(resp) {
+  return resp.json();
+}
+
+function displayNames(data) {
+  console.log(data.cats);
+  const cats = data.cats;
+  cats.forEach(getOneCatName);
+}
+
+function getOneCatName(cat) {
+  console.log(cat.name);
 }
