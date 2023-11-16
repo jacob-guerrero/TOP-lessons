f9ead2b1c4cec9464a9ea3fd784e92fae3226d6a
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-11-15 20:06:39 -0500

    Add common matchers and truthiness test

---
+++ js-full/testing/testing-basics/matchers.test.js
@@ -0,0 +1,38 @@
+// Exact equality
+test("two plus two is four", () => {
+  expect(2 + 2).toBe(4);
+});
+
+// Check the value of an object
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+// Opposite matcher
+test("adding positive numbers is not zero", () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
+
+// Test Truthiness
+test("null", () => {
+  const n = null;
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test("zero", () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
