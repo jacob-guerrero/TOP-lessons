11e2a8ec1ba583b46065037a2b0a9692342cb912
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-04-06 23:50:33 -0500

    Add examples choosing state structure

--- react-introduction/states-effects/src/App.jsx
+++ react-introduction/states-effects/src/App.jsx
@@ -2,6 +2,96 @@ import { useState } from "react";
 import sculptureList from "./data";
 import "./App.css";
 
+function Menu() {
+  const initialItems = [
+    { title: 'pretzels', id: 0 },
+    { title: 'crispy seaweed', id: 1 },
+    { title: 'granola bar', id: 2 },
+  ];
+
+  const [items, setItems] = useState(initialItems);
+  const [selectedId, setSelectedId] = useState(0);
+
+  const selectedItem = items.find(item =>
+    item.id === selectedId
+  );
+
+  function handleItemChange(id, e) {
+    setItems(items.map(item => {
+      if (item.id === id) {
+        return {
+          ...item,
+          title: e.target.value,
+        };
+      } else {
+        return item;
+      }
+    }));
+  }
+
+  return (
+    <>
+      <h2>What's your travel snack?</h2>
+      <ul>
+        {items.map((item, index) => (
+          <li key={item.id}>
+            <input
+              value={item.title}
+              onChange={e => {
+                handleItemChange(item.id, e)
+              }}
+            />
+            {' '}
+            <button onClick={() => {
+              setSelectedId(item.id);
+            }}>Choose</button>
+          </li>
+        ))}
+      </ul>
+      <p>You picked {selectedItem.title}.</p>
+    </>
+  );
+}
+
+
+function Form() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const fullName = firstName + ' ' + lastName;
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  return (
+    <>
+      <h2>Let's check you in</h2>
+      <label>
+        First name:{' '}
+        <input
+          value={firstName}
+          onChange={handleFirstNameChange}
+        />
+      </label>
+      <label>
+        Last name:{' '}
+        <input
+          value={lastName}
+          onChange={handleLastNameChange}
+        />
+      </label>
+      <p>
+        Your ticket will be issued to: <b>{fullName}</b>
+      </p>
+    </>
+  );
+}
+
 function TrafficLight() {
   const [walk, setWalk] = useState(true);
 
@@ -156,6 +246,8 @@ function App() {
       <ColorsElement></ColorsElement>
       <Person></Person>
       <TrafficLight></TrafficLight>
+      <Form></Form>
+      <Menu></Menu>
     </>
   );
 }
