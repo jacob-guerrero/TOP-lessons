7e8b83e0fbf8f333607a10ac27ed8fe0e35d6e67
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-06-18 17:40:09 -0500

    Add example using Effects

---
+++ react-introduction/side-effects/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Clock from "./components/Clock";
+
+function App() {
+
+  return (
+    <>
+      <Clock></Clock>
+    </>
+  )
+}
+
+export default App
