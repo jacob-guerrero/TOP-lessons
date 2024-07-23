7d5234014ae345953f9ca5572a22e2b0ae41846e
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-07-22 19:42:37 -0500

    Add test app

---
+++ react-testing/src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => <h1>Our First Test</h1>;
+
+export default App
