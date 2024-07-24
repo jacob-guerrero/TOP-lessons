bcc3e0bf9a672dc7762e999b674171730a539c11
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-07-23 20:14:37 -0500

    Add test simulating user events

--- react-testing/src/App.jsx
+++ react-testing/src/App.jsx
@@ -3,6 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const App = () => <h1>Our First Test</h1>;
+const App = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+};
 
 export default App
--- react-testing/src/App.test.jsx
+++ react-testing/src/App.test.jsx
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 describe("something truthy and falsy", () => {
@@ -23,9 +24,30 @@ describe("App", () => {
   });
 });
 
-describe("App component", () => {
-    it("renders correct heading", () => {
-      render(<App />);
-      expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
-    });
+/* First query test */
+/* describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
   });
+}); */
+
+/* Simulating user events */
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+  });
+});
