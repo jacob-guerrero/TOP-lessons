88122ac62c103596063602110eeaf3eb74dd2f48
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-07-31 11:54:43 -0500

    Add router example

---
+++ react-router/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <a href="profile">Profile page</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
