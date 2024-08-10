e4da4205995ea7a8a6a23de693a8bc06b4100c79
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-08-10 18:53:20 -0500

    Add ErrorPage content and Handle bad urls

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Profile from "./components/Profile";
+import ErrorPage from "./components/ErrorPage";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>,
   },
   {
     path: "profile/:name",
