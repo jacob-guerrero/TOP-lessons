d6f7a0408945b90b2478869d6378c345f14b8984
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-08-10 18:47:50 -0500

    Add dynamic segments

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Profile from "./components/Profile";
-import Spinach from "./components/Spinach";
-import Popeye from "./components/Popeye";
-import DefaultProfile from "./components/DefaultProfile";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -14,22 +11,8 @@ const router = createBrowserRouter([
     element: <App></App>,
   },
   {
-    path: "profile",
+    path: "profile/:name",
     element: <Profile></Profile>,
-    children: [
-      {
-        index: true,
-        element: <DefaultProfile></DefaultProfile>,
-      },
-      {
-        path: "spinach",
-        element: <Spinach></Spinach>,
-      },
-      {
-        path: "popeye",
-        element: <Popeye></Popeye>,
-      },
-    ],
   },
 ]);
 
