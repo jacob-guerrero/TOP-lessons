5b367dce2b787d2e1986fe4dae72caf35dc4dc36
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-08-14 20:19:45 -0500

    Add ProfileManager with useful links

---
+++ react-router/src/components/ProfileManager.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const ProfileManager = () => {
+  return (
+    <>
+      <h1>Hello from the profile Manager!</h1>
+      <p>There are some options of links:</p>
+      <ul>
+        <li>
+          <Link to="popeye">Popeye</Link>
+        </li>
+        <li>
+          <Link to="spinach">Spinach</Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default ProfileManager;
--- react-router/src/routes.jsx
+++ react-router/src/routes.jsx
@@ -1,12 +1,17 @@
 import App from "./App";
 import Profile from "./components/Profile";
 import ErrorPage from "./components/ErrorPage";
+import ProfileManager from "./components/ProfileManager";
 
 const routes = [
   {
     path: "/",
     element: <App></App>,
     errorElement: <ErrorPage></ErrorPage>,
+  },
+  {
+    path: "profile",
+    element: <ProfileManager></ProfileManager>,
   },
   {
     path: "profile/:name",
