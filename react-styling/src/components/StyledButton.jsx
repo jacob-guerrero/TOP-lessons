face40ee5215428b9087014e5cee1ffb68b1e5f8
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-08-30 12:03:00 -0500

    Add css composition

--- react-styling/src/components/StyledButton.jsx
+++ react-styling/src/components/StyledButton.jsx
@@ -1,5 +1,12 @@
 import styles from "../styles/Button.module.css";
 
-export default function StyledButton() {
-  return <button className={styles.btn}>Submit</button>;
+export default function StyledButton({ type = "primary", label = "Button" }) {
+  const type2 = "secondary";
+  const label2 = "Button2";
+  return (
+    <>
+      <button className={styles[type]}>{label}</button>
+      <button className={styles[type2]}>{label2}</button>
+    </>
+  );
 }
