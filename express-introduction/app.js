e2ab830bc4acd5e26c42f907ec194decf433399c
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-10-26 21:05:33 -0500

    Add routes in a basic informational site

--- express-introduction/app.js
+++ express-introduction/app.js
@@ -1,7 +1,66 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const options = {
+    root: path.join(__dirname),
+  };
+  const fileName = "index.html";
+
+  res.sendFile(fileName, options, (err) => {
+    if (err) {
+      console.log("Error: ", err);
+    } else {
+      console.log("Sent: ", fileName);
+    }
+  });
+});
+
+app.get("/about(.html)?", (req, res) => {
+  const options = {
+    root: path.join(__dirname),
+  };
+  const fileName = "about.html";
+
+  res.sendFile(fileName, options, (err) => {
+    if (err) {
+      console.log("Error: ", err);
+    } else {
+      console.log("Sent: ", fileName);
+    }
+  });
+});
+
+app.get("/contact-me(.html)?", (req, res) => {
+  const options = {
+    root: path.join(__dirname),
+  };
+  const fileName = "contact-me.html";
+
+  res.sendFile(fileName, options, (err) => {
+    if (err) {
+      console.log("Error: ", err);
+    } else {
+      console.log("Sent: ", fileName);
+    }
+  });
+});
+
+app.get("/*", (req, res) => {
+  const options = {
+    root: path.join(__dirname),
+  };
+  const fileName = "404.html";
+
+  res.sendFile(fileName, options, (err) => {
+    if (err) {
+      console.log("Error: ", err);
+    } else {
+      console.log("Sent: ", fileName);
+    }
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
