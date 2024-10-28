296864b1163f4eecc4bc9e1cc54af70aa766647d
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-10-27 23:58:17 -0500

    Add basic routes example

---
+++ express-routes/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const authorRouter = require("./routes/authorRouter");
+
+app.use("/authors", authorRouter);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}`);
+});
