b07dc63fbfb027d29f85e0167eb1c5e2a9e13c89
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-11-12 12:13:47 -0500

    Add local variables

--- express-views/app.js
+++ express-views/app.js
@@ -5,7 +5,17 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.send("Home"));
+const links = [
+  { href: "/", text: "Home" },
+  {
+    href: "about",
+    text: "About",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { links: links });
+});
 
 const PORT = 3000;
 
