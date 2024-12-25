866f9e09b2b4beda525434f30183451ba9906761
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-12-25 17:13:13 -0500

    Populate Db and create env variables

---
+++ express-postgresql/db/populatedb.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+require("dotenv").config();
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS usernames (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR (255)
+);
+
+INSERT INTO usernames (username)
+VALUES
+    ('Bryan'),
+    ('Odin'),
+    ('Damon');
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: process.env.connectionString,
+  });
+
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
