0054adee5085f8c1552960daea567432158b9303
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2025-02-21 10:34:46 -0500

    Create log-in form and modify logic
    Create log-in form in the index file (root)
    Add log-in logic
    Add log-out logic
    Set up local variables (currentUser) throughout
    the app

--- express-auth/app.js
+++ express-auth/app.js
@@ -18,21 +18,6 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
-
-app.get("/sign-up", (req, res) => res.render("sign-up-form"));
-app.post("/sign-up", async (req, res, next) => {
-  try {
-    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
-      req.body.username,
-      req.body.password,
-    ]);
-    res.redirect("/");
-  } catch (err) {
-    return next(err);
-  }
-});
-
 /* Set up LocalStrategy */
 passport.use(
   new LocalStrategy(async (username, password, done) => {
@@ -72,6 +57,44 @@ passport.deserializeUser(async (id, done) => {
   } catch (err) {
     done(err);
   }
+});
+
+/* Set and access currentUser */
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+/* Views and middleware */
+app.get("/", (req, res) => res.render("index", { user: req.user }));
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+      req.body.username,
+      req.body.password,
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
 
 const PORT = 3000;
